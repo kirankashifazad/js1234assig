ed3c99e2db01554818bc6b22a7cf9419289ca1ac
kiran kashif azad <kirankashifazad@gmail.com>
2024-03-08 22:58:22 +0500

    work is done

---
+++ aap.js
@@ -0,0 +1,39 @@
+  
+  
+ <h1> assignment no 1</h1>
+  
+  
+  ("Hello World")
+alert("please! Enter the walid password")
+alert("Wellcome to js....\nHappt coading")
+alert("Wellcome to js land.")
+alert("Happy coading \n this page from creating additional dialoge")
+
+console.log("i can run js my web brouser consol")
+
+
+ assignment no 2
+
+
+
+var userName = "ali"
+var myName = "kiran Azad"
+var massage = "Hello World"
+
+alert(massage)
+var name = "jhon Doe"
+alert(name)
+var age = "15 years old"
+alert(age)
+var mass = "certified mobile Application Development"
+alert(mass)
+var food =("pizza\npizz\npiz\npi\np")
+alert(food)
+var email =("kiran azad@gmail.com")
+alert(email)
+var book = (" i am traing to learnFrom the Book A Smart Way To learn java Script")
+alert (book)
+var add = ("Yeah! i can Write HTML Content from java Script"+"<br>")
+document.write(add)
+var design =("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+document.write(design)
---
+++ app.js
@@ -0,0 +1,129 @@
+//  assigment no 3
+
+var age = ("i am 27 years old")
+alert(age)
+var time =("you are visit 14 times this site")
+alert(time)
+var birthYear = ("My birth year is 1997 " +"<br>")
+document.write(birthYear)
+var visitrName = ("<h1>john Doe ,")
+var quantity = ("Order 5 T_shirts, ")
+var products = (" koi bhi store.com </h1>")
+document.write (visitrName +quantity + products )
+
+
+// assigment no 4
+
+
+// var threeVariable = $ ,_ ,content,
+// five leegal variabe = 
+// var name = ("acgvjh")
+// var $price = (1214)
+// var class2 = ("dnkj")
+// var _school = ("school name")
+// var w3school = (23750947)
+
+
+// // five illeegle var is 
+//  var 123424 =,
+//  var @dfre =,
+//  var %fmgl;t=
+//  var *fnglket =
+// var &kfmlkd =
+
+var heading = ("Rules for naming JS variables")
+document.write (heading)
+var one = ("Variable names can only contain,numbers,$ and _")
+var two =("Variables must begin with a $ _ or name")
+var three = ("Variable names are case sensitive")
+var four = ("Variable names should not be JS keywords")
+document.write(one,"<br>"+two,"<br>" +three,"<br>"+four)
+
+
+//  assigment no 5
+
+
+var num1 = 5
+var num2 = 3
+document.write ("sub of 3 and 5 is ",num1-num2,"<br>")
+
+var num1 = 5
+var num2 = 3
+document.write ("mul of 3 and 5 is ",num1*num2,"<br>")
+
+var num1 = 5
+var num2 = 3
+document.write ("devide of 3 and 5 is ",num1/num2,"<br>")
+
+var num1 = 5
+var num2 = 3
+document.write ("mod of 3 and 5 is ",num1%num2,"<br>")
+
+var num1 = 5
+var num2 = 3
+document.write ("sum of 3 and 5 is ",num1+num2,"<br.")
+
+
+var num 
+ var a = 5
+document.write ("initilize value",a,"<br>")
+document.write ("after increament a value is " , ++a,"<br>")
+document.write ("after addition  7 a value is ", a + 7  ,"<br>")
+document.write ("after dicrement  a value is " ,a + a-- ,"<br>")
+var res = (a /3)
+  document.write( "after devide by 3 res is ",res)
+   
+
+
+var movie_ticket = 600
+var quantity = 5
+var res = movie_ticket* quantity
+document.write( "the cost of total movie ticket",res )
+
+
+
+for (var i=1; i<=10 ;i++)
+document.write (4 , "*" ,i,"=",i*4 ,"<br>") 
+
+
+
+var priceOne = 650 
+var priceTwo = 100
+var quantity1 = 3
+var quantity2 = 7
+var shippingCharges =100
+var res = ((priceOne *  quantity1 ) + (priceTwo * quantity2 ))
+document.write(" priceOne = 650","<br>","quantity1 = 3","<br>", "priceTwo = 100","<br>","quantity2 = 7","<br>","shiping charges = 100","<br>","var result is =" ,res+shippingCharges)
+
+
+
+var obt_mark = 804
+var tt_mark = 980
+var res = obt_mark/tt_mark*100
+document.write("obt_mark is =",obt_mark,"<br>","tt_mark is",tt_mark,"<br>","percentage is =",res,"<br>")
+
+
+
+var dollar = 10*104.80
+var riyal = 25*28 
+var res = dollar +  riyal 
+document.write ("total currency in PKR is  ",res,"<br>")
+
+
+var prog = 5
+var res = (prog +5) *10 /2
+document.write("add "+5,"<br>","multiply by 10","<br>","Divide the result by 2","<br>","res is ",res,"<br>")
+
+
+var currentYear = 2024
+var birthYear = 1997
+var result = currentYear - birthYear
+document.write("current year =",currentYear,"<br>","bearth year =",birthYear,"<br>"," your current age is =" ,result,"<br>")
+
+
+
+var current_age = 15
+var max_age = 65
+var exp_perDay = 3
+var res = (max_age - current_age)*exp_perDay
+document.write("current age =",current_age,"<br>","max age =",max_age,"<br>","expence per day",exp_perDay,"<br>","you need  ",res,"chocolate chip to old age","<br>")
